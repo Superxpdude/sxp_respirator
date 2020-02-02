e2d4821071d3f39a74de5cb7c17de1bbaa59346d
Superxpdude <superxpdude@gmail.com>
2020-02-02 13:06:32 -0600

    Initial commit.

---
+++ addons/respirator/config.cpp
@@ -0,0 +1,24 @@
+class cfgPatches
+{
+	class sxp_respirator
+	{
+		// Default BIS info
+		name = "SuperXP Respirator";
+		author = "Superxpdude";
+		url = "https://github.com/Superxpdude/sxp_respirator";
+		
+		requiredAddons[] = {
+			"A3_Data_F_Enoch",
+			"A3_Characters_F_Enoch",
+			"A3_Supplies_F_Enoch_Bags",
+			"ace_interaction"
+		};
+		
+		// CBA Info
+		authors[] = {"Superxpdude"};
+		version = 1.0;
+		versionStr = "1.0";
+		versionAr[] = {1.0.0};
+	};
+};
+
